6387568613b9b497a99cc7538b1629b4b005d08d
Aadesh Kulkarni <aadesh.kulkarni@ril.com>
2022-04-03 13:25:39 +0530

    formatting for mobile

--- client/src/Header.js
+++ client/src/Header.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Header({ setHelpStatus,setStatStatus }) {
   return (<header>
     <span style={{cursor:"pointer"}} onClick={() => setHelpStatus(true)}><img src="help-icon.svg" style={{ width: "32px", height: "32px" }} /></span>
-    <h1 style={{ padding: "1rem", letterSpacing: "2px" }}>
+    <h1 className="app-title" >
      | Wordle | 
       </h1>
       <span style={{marginRight:"2rem",cursor:"pointer"}} onClick={() => setStatStatus(true)}><img src="stats.svg" style={{ width: "32px", height: "32px" }} /></span>
